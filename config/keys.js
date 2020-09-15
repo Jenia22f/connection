6cb7848863112c1c5ec66d61bae9c72367fa797e
Jenia22f <Nofightnoglory22>
2020-09-15 17:37:46 +0300

    change auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost/deviceConnections'
+    mongoURI: 'mongodb://eugin1:appside22dev@167.172.176.156/deviceConnections'
+    // mongoURI: 'mongodb://167.172.176.156/deviceConnections'
 }
