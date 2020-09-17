c29b01e7fc4922b54e92268a774bea475cbdf6be
Jenia22f <Nofightnoglory22>
2020-09-17 10:21:43 +0300

    a

--- controllers/pinging.js
+++ controllers/pinging.js
@@ -6,13 +6,11 @@ module.exports.pinging = async function (req, res) {
     let newDuration;
     try {
         const device = await Ping.find({deviceHash: req.body.deviceHash})
-        console.log(device)
+
         if (device.length) {
         let date = Math.round(Date.now() / 60000)
 
             newDuration = date - device[0].startTime;
-        }
-            console.log(newDuration);
             const update = await Ping.findOneAndUpdate(
             {deviceHash: req.body.deviceHash},
              {duration: newDuration}
